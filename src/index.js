7a6cf67b84410f851a6cb6fd54420abc8444b791
jrshort89 <jrshort89@gmail.com>
2020-11-18 16:16:43 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+// your code here
+const createLi = (text) => {
+    const list = document.getElementById('tasks');
+    // const list = document.querySelector('ul');
+    const li = document.createElement('li');
+    li.innerText = text + "!";
+    li.style.color = document.getElementById('colors').value;
+    list.append(li);
+
+    li.addEventListener('click', (event) => {
+        event.target.remove();
+        event.preventDefault();
+    });
+
+    li.addEventListener('mouseleave', (event) => {
+        let data = prompt('Edit?');
+        event.target.innerText = data;
+    })
+
+}
+
+const listButton = document.getElementById('listButton');
+listButton.addEventListener('click', (event) => {
+    const input = document.getElementById("new-task-description").value + ' ' + document.getElementById("new-task").value;
+    createLi(input);
+    event.preventDefault();
 });
